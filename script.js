a772b8e013327d45b1ecc1994011666cc8f550f9
e-mandy <andymfrd02@gmail.com>
2025-05-04 11:08:29 +0100

    Mise en place d'une calculatrice - Step 1

---
+++ script.js
@@ -0,0 +1,63 @@
+let buttons = document.querySelectorAll('.calc-container .calc .buttons button')
+let resultInput = document.querySelector('.calc-container .calc .result')
+
+p = document.createElement('p')
+resultInput.appendChild(p)
+let value, prem, last, result, secondTime = false
+let operator
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', ()=>{
+        value = buttons[i].value
+        if(secondTime == true){
+            p.textContent = ""
+            secondTime = false
+        }
+        if(!isNaN(value)){
+            p.textContent += `${value}`
+            last = parseInt(p.textContent)
+        }else{
+            if(value !== "="){
+                prem = parseInt(p.textContent)
+                operator = value
+                last = undefined
+                secondTime = true
+            }
+        }
+
+        if(operator != undefined && last != undefined){
+            switch(operator){
+                case "+":
+                    result = last + prem
+                    break
+                
+                case "-":
+                    result = prem - last
+                    break
+
+                case "x":
+                    result = last * prem
+                    break
+
+                case "/":
+                    result = prem / last
+                    break
+            }
+
+            prem = result
+        }
+
+        if(value == "="){
+            if(prem == undefined){
+                p.textContent = `${last}`
+            }else{
+                p.textContent = `${result}`
+            }
+        }
+        
+
+            
+    })
+}
+
+
+
